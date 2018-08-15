bd26802d2d97adf07105810fb0e2adb2af9e8117
abhishekdhanerwal <abhishekdhanerwal@gmail.com>
2018-08-16 00:21:53 +0530

    some changes in api script

--- TestAuto/src/test/java/PageObjects/api.java
+++ TestAuto/src/test/java/PageObjects/api.java
@@ -1,6 +1,7 @@
 package PageObjects;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
@@ -11,85 +12,86 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 public class api {
-	
-	public static String getResponseData(String apiUrl){
+
+	public static String getResponseData(String apiUrl) {
 		String data = null;
 		Response rsp = RestAssured.get(apiUrl);
 
 		data = rsp.asString();
-		
+
 		return data;
-		
+
 	}
-	
-	public static boolean verifyJsonObjects(String expVal , String apiUrl){
-		
+
+	public static boolean verifyJsonObjects(String expVal, String apiUrl) {
+
 		boolean objectPresent = false;
 
 		JSONObject jsonObject = new JSONObject(getResponseData(apiUrl));
-		
+
 		Iterator<String> iterator = jsonObject.keys();
 
 		while (iterator.hasNext()) {
-			
+
 			String key = iterator.next();
-			
-			if(expVal.equals(key)){
-				
+
+			if (expVal.equals(key)) {
+
 				objectPresent = true;
 			}
-			
+
 		}
-		
+
 		return objectPresent;
-		
-		
+
 	}
-	
-	public static int check(JSONObject jsonObject,String objName) {
-		int count = 0;
-		
-			if (jsonObject.get(objName) instanceof JSONArray) {
-				JSONArray jsonArray = (JSONArray) jsonObject.get(objName);
-				count  = jsonArray.length();
-				
-				System.out.println("lenght= " + jsonArray.length());
-				
-				for (Object jsonObject2 : jsonArray) {
-					
-					check((JSONObject) jsonObject2,objName);
+
+	public static int check(JSONObject jsonObject, String objName, Integer count) {
+		int num = count;
+		Iterator<String> iterator = jsonObject.keys();
+		while (iterator.hasNext()) {
+			String key = iterator.next();
+			if (key.equals(objName)) {
+				if (jsonObject.get(key) instanceof JSONObject) {
+					JSONObject dataObj = jsonObject.getJSONObject(key);
+					Iterator<String> iterator2 = dataObj.keys();
+					while (iterator2.hasNext()) {
+						String keyData = iterator2.next();
+						if (keyData.equals("data") && dataObj.get(keyData) instanceof JSONArray) {
+							JSONArray jsonArray = (JSONArray) dataObj.get(keyData);
+							num = num + jsonArray.length();
+
+							System.out.println("lenght required = " + jsonArray.length() + " " + objName + " " + key);
+
+						}
+					}
 				}
 
-			} else {
-				if (jsonObject.get(objName) instanceof JSONObject)
-					check(jsonObject.getJSONObject(objName),objName);
-				else
-					System.out.println(jsonObject.get(objName));
 			}
-			
-			return count;
 		}
-		
-		
-	
-	
-	public static boolean verifyJsonObjectDataCount(String apiUrl,String objName,String expCountVal){
+		return num;
+	}
+
+	public static boolean verifyJsonObjectDataCount(String apiUrl, String objName, String expCountVal) {
 		boolean countMatch = false;
 		int dataLenActual = 0;
 		int expCount = Integer.parseInt(expCountVal);
-		
-        JSONObject jsonObject = new JSONObject(getResponseData(apiUrl));
-		
-		dataLenActual = check(jsonObject,objName);
 
-		if(expCount==dataLenActual){
-			
+		JSONObject jsonObject = new JSONObject(getResponseData(apiUrl));
+
+		int count = 0;
+		dataLenActual = check(jsonObject, objName, count);
+
+		System.out.println("Data count is" + dataLenActual);
+
+		if (expCount == dataLenActual) {
+
 			countMatch = true;
-			
+
 		}
-		
+
 		return countMatch;
-		
+
 	}
 
 }
--- TestAuto/src/test/java/TestCases/apiTest.java
+++ TestAuto/src/test/java/TestCases/apiTest.java
@@ -21,14 +21,15 @@ import Utility.util;
 
 
 
-public class apiTest extends Base{
+public class apiTest{
 	
 	// String apiUrl = "https://api.darksky.net/forecast/8874af6be307b40e41b535ce8eb32979/37.8267,-122.4233";
 	
 	 @DataProvider(name = "Api Object verification")
 	  public static Object[][] jsonObjects() {
 	 
-	        return new Object[][] { {"firstObj","latitude"} ,{"SecondObj","longitude"}};
+	        return new Object[][] { {"firstObj","latitude"} ,{"SecondObj","longitude"},{"ThirdObj","timezone"} ,
+	        	{"FourthObj","currently"},{"fifthObj","minutely"} ,{"sixthObj","hourly"},{"seventhObj","daily"} ,{"eightObj","flags"},{"ninthObj","offset"}};
 	 
 	  }
 	 
@@ -53,7 +54,7 @@ public class apiTest extends Base{
 	 @DataProvider(name = "Api data count")
 	  public static Object[][] jsonDataCount() {
 	 
-	        return new Object[][] { {"minutely","61"} ,{"hourly","47"}, {"daily","8"}};
+	        return new Object[][] { {"minutely","61"} ,{"hourly","49"}, {"daily","8"}};
 	 
 	  }
 	 
@@ -74,45 +75,4 @@ public class apiTest extends Base{
 	 
 	 
 
-	/*@Test(dataProvider = "Api Object verification")
-	public void verifyResponseObjects(String jsonExpObj) {
-
-		Response rsp = RestAssured
-				.get("https://api.darksky.net/forecast/8874af6be307b40e41b535ce8eb32979/37.8267,-122.4233");
-
-		String respData = rsp.asString();
-
-		System.out.println("Response data is: " + respData);
-
-		JSONObject jsonObject = new JSONObject(respData);
-
-		check(jsonObject);
-		
-		
-
-	}
-
-	private void check(JSONObject jsonObject) {
-		Iterator<String> iterator = jsonObject.keys();
-
-		while (iterator.hasNext()) {
-			String key = iterator.next();
-			System.out.print(key + " ");
-			if (jsonObject.get(key) instanceof JSONArray) {
-				JSONArray jsonArray = (JSONArray) jsonObject.get(key);
-				System.out.println("lenght= " + jsonArray.length());
-				for (Object jsonObject2 : jsonArray) {
-					check((JSONObject) jsonObject2);
-				}
-
-			} else {
-				if (jsonObject.get(key) instanceof JSONObject)
-					check(jsonObject.getJSONObject(key));
-				else
-					System.out.println(jsonObject.get(key));
-			}
-			System.out.println();
-		}
-	}
-*/
 }
